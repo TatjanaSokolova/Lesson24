ed83a37c454411800d195bc7ff81cec322c3546c
TatjanaSokolova <tsvm@inbox.lv>
2021-08-22 17:01:21 +0300

    Graphs. Class work

---
+++ src/Lesson23.java
@@ -0,0 +1,99 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Lesson23 {
+
+    public static void main(String[] args) {
+        Graph myGraph = createGraph();
+
+        // 1. Print all the vertices
+        for (Vertex myVertex : myGraph.getAdjVertices().keySet()) {
+            System.out.println(myVertex.label);
+        }
+
+        // 2. Create an Airport Graph
+        Graph myAirportGraph = createAirportGraph();
+
+        // 3. Print all the Airports
+        for (Vertex myVertex : myAirportGraph.getAdjVertices().keySet()) {
+            System.out.println(myVertex.label);
+        }
+
+        // 4. How many direct flights are there in Graph
+
+        int flightCounter = 0;
+        for (Vertex myVertex : myAirportGraph.getAdjVertices().keySet()) {
+            for (Vertex flight : myAirportGraph.getAdjVertices(myVertex.label)
+            ) {
+                flightCounter++;
+            }
+        }
+        System.out.println("There are " + flightCounter + " in total");
+
+        // 5. Direct flights from Cape Town
+        System.out.println("--- Direct Flights from Cape Town:");
+        for (Vertex directFlight :
+                myAirportGraph.getAdjVertices("Cape Town")) {
+            System.out.println(directFlight.label);
+        }
+
+        // 6. Direct flights count from Jo'burg airport
+        flightCounter = 0;
+        System.out.println("--- Direct Flights count from Jo'Burg:");
+        for (Vertex directFlight :
+                myAirportGraph.getAdjVertices("Jo'Burg")) {
+            flightCounter++;
+        }
+
+        System.out.println("Direct flights from Jo'Burg: " + flightCounter);
+
+    }
+
+    static Graph createGraph() {
+        Graph graph = new Graph();
+        graph.addVertex("Bob");
+        graph.addVertex("Alice");
+        graph.addVertex("Mark");
+        graph.addVertex("Rob");
+        graph.addVertex("Maria");
+        graph.addEdge("Bob", "Alice");
+        graph.addEdge("Bob", "Rob");
+        graph.addEdge("Alice", "Mark");
+        graph.addEdge("Rob", "Mark");
+        graph.addEdge("Alice", "Maria");
+        graph.addEdge("Rob", "Maria");
+        return graph;
+    }
+
+    static Graph createAirportGraph() {
+        Graph graph = new Graph();
+        graph.addVertex("Jo'Burg");
+        graph.addVertex("Livingstone");
+        graph.addVertex("Victoria Falls");
+        graph.addVertex("Harare");
+        graph.addVertex("Windhoek");
+        graph.addVertex("Mauritius");
+        graph.addVertex("Durbs");
+        graph.addVertex("East London");
+        graph.addVertex("Cape Town");
+        graph.addVertex("PE");
+        graph.addEdge("Livingstone", "Jo'Burg");
+        graph.addEdge("Victoria Falls", "Jo'Burg");
+        graph.addEdge("Harare", "Jo'Burg");
+        graph.addEdge("Mauritius", "Jo'Burg");
+        graph.addEdge("Windhoek", "Jo'Burg");
+        graph.addEdge("Durbs", "Jo'Burg");
+        graph.addEdge("East London", "Jo'Burg");
+        graph.addEdge("PE", "Jo'Burg");
+        graph.addEdge("Cape Town", "Jo'Burg");
+        graph.addEdge("Durbs", "Cape Town");
+        graph.addEdge("Durbs", "PE");
+        graph.addEdge("Cape Town", "East London");
+        graph.addEdge("PE", "Cape Town");
+
+
+        return graph;
+    }
+
+}
---
+++ src/Vertex.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Vertex {
+    String label;
+    Vertex(String label) {
+        this.label = label;
+    }
+    // equals and hashCode
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vertex vertex = (Vertex) o;
+        return Objects.equals(label, vertex.label);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(label);
+    }
+
+
+}
+
